47cd2041967c8e640e09ceeddbb7c6a95e7442e3
vadimeladii <vadimeladii@gmail.ru>
2017-12-16 12:19:03 +0200

    init project

---
+++ src/main/java/md/utm/fcim/eureka/DataWarehouseEurekaApplication.java
@@ -0,0 +1,16 @@
+package md.utm.fcim.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DataWarehouseEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataWarehouseEurekaApplication.class, args);
+	}
+}
